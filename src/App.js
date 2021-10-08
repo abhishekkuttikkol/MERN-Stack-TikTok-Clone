ed85208d480defba00d659e49ee48ceb031aa421
Abhishek <abhishekkuttikkol007@gmail.com>
2021-10-08 20:32:50 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./Components/Video/Video";
-import Axios from './axios'
+import Axios from "./axios";
 
 function App() {
-  const [videos, setvideos] = useState([])
+  const [videos, setvideos] = useState([]);
   useEffect(() => {
-    async function getData(){
-      const response = await Axios.get('/v2/posts')
-      setvideos(response.data)
-      return response
+    async function getData() {
+      const response = await Axios.get("/v2/posts");
+      setvideos(response.data);
+      return response;
     }
-    getData()
-  }, [])
-  console.log(videos)
+    getData();
+  }, []);
+  console.log(videos);
   return (
     <div className="app">
       <div className="app__videos">
-        {videos.map((video)=>(
+        {videos.map((video) => (
           <Video
-          url={video.url}
-          channel={video.channel}
-          description={video.description}
-          song={video.song}
-          likes={video.likes}
-          shares={video.shares}
-          messages={video.messages}
-        />
+            url={video.url}
+            channel={video.channel}
+            description={video.description}
+            song={video.song}
+            likes={video.likes}
+            shares={video.shares}
+            messages={video.messages}
+          />
         ))}
       </div>
     </div>
